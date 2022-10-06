12419837dbbef485c7423bbb12b437c6f6461b8d
Damar Yosa Aji <damaryosaaji@gmail.com>
2022-10-06 20:36:51 +0700

    jkn

---
+++ android/app/src/main/kotlin/com/example/bpjs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bpjs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/signin/signin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SignIn'),
+      ),
+    );
+  }
+}
